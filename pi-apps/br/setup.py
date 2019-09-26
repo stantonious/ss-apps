8e011328a06cf539899e5672abb35989dfd39245
Bryan Staley <bryan.staley@ndpgroup.com>
2019-09-26 17:45:36 -0600

    Adding led app

--- pi-apps/br/setup.py
+++ pi-apps/br/setup.py
@@ -23,6 +23,7 @@ setup(name='ss_br_apps',
       author_email='bryan.w.staley@gmail.com',
       scripts=['audio_recorder.py',
                'backoff.py',
+               'leds.py',
                'notification.py',
                'embedding_recorder.py'],
       packages=['app_utils']
