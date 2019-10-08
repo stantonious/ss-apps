35d1458fd0e7894e8a07b7562f7928dcf21758b0
Bryan Staley <bryan.staley@ndpgroup.com>
2019-10-08 11:06:57 -0600

    Cleaning up

--- pi-apps/inf/__init__.py
+++ pi-apps/inf/__init__.py
@@ -3,20 +3,3 @@ __author__ = "Bryan Staley"
 __copyright__ = "Copyright 2019"
 __credits__ = []
 __license__ = "GPL"
-
-import logging
-import sys
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
-# add file handler
-fh = logging.FileHandler('/var/log/ss/ss.log')
-fh.setLevel(logging.DEBUG)
