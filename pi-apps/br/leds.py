44407a8a95c9469225e6cd712bce02f46f4efc4e
Bryan Staley <bryan.staley@ndpgroup.com>
2019-10-08 18:13:26 -0600

    Cleaning up

--- pi-apps/br/leds.py
+++ pi-apps/br/leds.py
@@ -19,13 +19,15 @@ from business_rules import variables, actions, run_all, fields
 parser = argparse.ArgumentParser(
     description='send notification for provided inference')
 
-parser.add_argument('-s', '--sleep-action-duration', type=int, required=True)
-parser.add_argument('-e', '--event-window', type=int, required=True)
-parser.add_argument('-t', '--event-num-threshold', type=int, required=True)
-parser.add_argument('-c', '--confidence-threshold', type=float, required=True)
-parser.add_argument('-i', '--index', type=int, required=True)
-parser.add_argument('-l', '--led-id', type=str, required=True)
-parser.add_argument('-C', '--led-color', type=str, required=True)
+parser.add_argument('--conf1', type=float, required=True)
+parser.add_argument('--conf2', type=float, required=True)
+parser.add_argument('--conf3', type=float, required=True)
+parser.add_argument('--idx1', type=int, required=True)
+parser.add_argument('--idx2', type=int, default=-1)
+parser.add_argument('--idx3', type=int, default=-1)
+parser.add_argument('--led-color1', type=str, required=True)
+parser.add_argument('--led-color2', type=str, required=True)
+parser.add_argument('--led-color3', type=str, required=True)
 
 api_key = os.environ.get('SS_API_KEY')
 
@@ -38,17 +40,40 @@ class NotificationActs(base.InferenceActs):
 
     @actions.rule_action(params={'idx': fields.FIELD_NUMERIC,
                                  'color': fields.FIELD_TEXT})
-    def change_led_color(self,
-                         idx,
-                         color):
+    def paint_strip(self,
+                    c1,
+                    c2,
+                    c3):
         from colour import Color
-        if color:
-            c = Color(color)
+        c1 = Color(c1) or None
+        c2 = Color(c2) or None
+        c3 = Color(c3) or None
 
+        if c1:
+            self.dev.set_pixel(1,
+                               int(c1.red * 255),
+                               int(c1.green * 255),
+                               int(c1.blue * 255))
+        else:
             self.dev.set_pixel(idx,
-                               int(c.red * 255),
-                               int(c.green * 255),
-                               int(c.blue * 255))
+                               int(0),
+                               int(0),
+                               int(0))
+        if c2:
+            self.dev.set_pixel(2,
+                               int(c2.red * 255),
+                               int(c2.green * 255),
+                               int(c2.blue * 255))
+        else:
+            self.dev.set_pixel(idx,
+                               int(0),
+                               int(0),
+                               int(0))
+        if c3:
+            self.dev.set_pixel(3,
+                               int(c3.red * 255),
+                               int(c3.green * 255),
+                               int(c3.blue * 255))
         else:
             self.dev.set_pixel(idx,
                                int(0),
@@ -60,44 +85,40 @@ class NotificationActs(base.InferenceActs):
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    tracked_idx = dict(name='idx',
-                       operator='equal_to',
-                       value=args.index)
-    cnt_exceeded = dict(name='cnt',
-                        operator='greater_than',
-                        value=args.event_num_threshold)
-    inf_thresh = dict(name='last_conf',
-                      operator='greater_than',
-                      value=args.confidence_threshold)
+    inf1_thresh = dict(name='last_conf1',
+                       operator='greater_than',
+                       value=args.conf1)
+    inf2_thresh = dict(name='last_conf2',
+                       operator='greater_than',
+                       value=args.conf2)
+    inf3_thresh = dict(name='last_conf3',
+                       operator='greater_than',
+                       value=args.conf3)
 
-    change_led = dict(name='change_led_color',
-                      params=dict(idx=int(args.led_id),
-                                  color=args.led_color))
+    not_inf1_thresh = dict(name='last_conf1',
+                           operator='less_than',
+                           value=args.conf1)
+    not_inf2_thresh = dict(name='last_conf2',
+                           operator='less_than',
+                           value=args.conf2)
+    not_inf3_thresh = dict(name='last_conf3',
+                           operator='less_than',
+                           value=args.conf3)
+
+    paint_strip = dict(name='paint_strip',
+                       params=dict(c1=arg.led_color1,
+                                   c2=arg.led_color2,
+                                   c3=arg.led_color3))
 
     off_led = dict(name='change_led_color',
                    params=dict(idx=int(args.led_id),
                                color=None))
 
-    reset_act_win = dict(name='reset_act_window',
-                         params=dict(duration=args.sleep_action_duration))
-    reset_win = dict(name='reset_window',
-                     params=dict(duration=args.event_window))
-
     rules = [
         {'conditions': {
-            'all': [inf_thresh]
+            'any': [inf1_thresh, inf2_thresh, inf3_thresh, not_inf1_thresh, not_inf2_thresh, not_inf3_thresh]
         },
-            'actions':[base.inc_cnt]
-        },
-        {'conditions': {
-            'all': [tracked_idx, cnt_exceeded, base.act_win]
-        },
-            'actions':[change_led, reset_act_win, base.reset_cnt]
-        },
-        {'conditions': {
-            'all': [base.window_elapsed]
-        },
-            'actions':[reset_win, base.reset_cnt, off_led]
+            'actions':[paint_strip]
         }
     ]
 
@@ -116,19 +137,19 @@ if __name__ == '__main__':
     def _callback(ch, method, properties, body):
         try:
             d = json.loads(body)
-            idx = np.argwhere(np.asarray(d['idxs']) == args.index)
-            if idx.shape[0] < 1:
-                print ('no receiving inference for class idx', args.index)
-                sys.exit()
-
-            conf = d['inferences'][idx[0, 0]]
-
-            inf.last_conf = conf
-            inf.embeddings = np.asarray(d['embeddings'], dtype=np.uint8)
+            idx1 = np.argwhere(np.asarray(d['idxs']) == args.index)
+            if idx1.shape[0] > 1:
+                inf.last_conf1 = d['inferences'][idx1[0, 0]]
+            idx2 = np.argwhere(np.asarray(d['idxs']) == args.index)
+            if idx2.shape[0] > 1:
+                inf.last_conf2 = d['inferences'][idx2[0, 0]]
+            idx3 = np.argwhere(np.asarray(d['idxs']) == args.index)
+            if idx3.shape[0] > 1:
+                inf.last_conf3 = d['inferences'][idx3[0, 0]]
             inf.time = d['time']
 
             run_all(rule_list=rules,
-                    defined_variables=base.InferenceVars(inf, 3),
+                    defined_variables=base.InferenceVars(inf),
                     defined_actions=NotificationActs(
                         inf, led_device=led_device),
                     stop_on_first_trigger=False)
