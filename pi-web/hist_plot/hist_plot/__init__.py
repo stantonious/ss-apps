a0fda5b5459956551d433ca4f8322ba9b08ca8d6
Bryan Staley <bryan.staley@ndpgroup.com>
2019-12-26 14:59:41 -0700

    hist plot

---
+++ pi-web/hist_plot/hist_plot/__init__.py
@@ -0,0 +1,11 @@
+""" Hist Plot package """
+__author__ = "Bryan Staley"
+__copyright__ = "Copyright 2019"
+__credits__ = []
+__license__ = "GPL"
+from flask import Flask
+import logging
+import sys
+app = Flask(__name__)
+
+from . import routes
