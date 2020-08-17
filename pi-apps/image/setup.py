14525a1a14cdb939c2a25f4508f633a2c86a98bc
bstaley <bryan.staley@ndpgroup.com>
2020-08-17 17:15:27 -0600

    Add pycamera support

---
+++ pi-apps/image/setup.py
@@ -0,0 +1,25 @@
+""" image setup """
+__author__ = "Bryan Staley"
+__copyright__ = "Copyright 2019"
+__credits__ = []
+__license__ = "GPL"
+
+from distutils.core import setup
+
+version = '1.0'
+
+setup(name='ss_image_apps',
+      version=version,
+      install_requires=[
+          'numpy',
+          'pika',
+          'picamera'
+      ],
+      description='''br apps.
+                 ''',
+      author='Bryan Staley',
+      author_email='bryan.w.staley@gmail.com',
+      scripts=[
+          'smile.py',],
+      packages=[]
+      )
