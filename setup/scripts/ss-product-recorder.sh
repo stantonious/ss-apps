4db2a20584cf6328f9176810a75eddc231564727
bstaley <bryan.staley@ndpgroup.com>
2021-01-15 13:20:52 -0700

    Adding Buzz services

--- setup/scripts/ss-product-recorder.sh
+++ setup/scripts/ss-product-recorder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /home/pi/venvs/ss/bin/activate
-product_recorder.py  --archive-dir $1 --idx $2 --confidence $3 --product=mel inf emb
+product_recorder.py  --archive-dir $1 --idx $2 --confidence $3 --product mel inf emb
